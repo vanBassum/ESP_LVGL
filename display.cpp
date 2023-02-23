13ca72f1b1073d6a821385825ec10ab5a9871de3
Bas Visscher <bas@koolecontrols.nl>
2023-02-23 13:33:45 +0100

    Added specific monochrome implementation

---
+++ display.cpp
@@ -0,0 +1,74 @@
+#include "display.h"
+
+
+esp_err_t ESP_LVGL::Display::Init(GLCD_Mono* glcdMonochrome)
+{
+	glcdMono = glcdMonochrome;
+	if (glcdMono == NULL)
+		return ESP_FAIL;
+			
+	uint32_t width = glcdMono->GetWidth();
+	uint32_t height = glcdMono->GetHeight();
+			
+	bufferSize = width * 10;
+	buffer = (uint8_t*)malloc(bufferSize);
+	if (buffer == NULL)
+		return ESP_FAIL;
+			
+	lv_disp_draw_buf_init(&disp_buf, buffer, NULL, bufferSize);
+	lv_disp_drv_init(&disp_drv);			
+	disp_drv.draw_buf = &disp_buf;			
+	disp_drv.hor_res = width;
+	disp_drv.ver_res = height;
+	disp_drv.user_data = this;
+	disp_drv.flush_cb = [](lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p) {
+		Display* instance = (Display*)disp_drv->user_data;
+		instance->Flush_cb_mono(disp_drv, area, color_p);
+		lv_disp_flush_ready(disp_drv);
+	};
+	
+	//Always write complete lines.
+	disp_drv.rounder_cb = [](struct _lv_disp_drv_t * disp_drv, lv_area_t * a) {	
+		a->x1 = 0;
+		a->x2 = 240;	
+	};
+	
+	//disp_drv.set_px_cb = [](struct _lv_disp_drv_t * disp_drv, uint8_t * buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y, lv_color_t color, lv_opa_t opa) {
+	// https://maldus512.medium.com/porting-littlevgl-for-a-monochrome-display-6c7be58851ce
+	// Not required
+	//};
+
+	disp = lv_disp_drv_register(&disp_drv); 
+	return ESP_OK;
+}
+
+
+void ESP_LVGL::Display::Flush_cb_mono(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p)
+{
+	if (glcdMono == NULL)
+		return;
+	int32_t x, y;
+	//Rounder ensures a whole line always needs to be updated.
+	uint8_t buf[30];	//30 columns
+	for (y = area->y1; y <= area->y2; y++) 
+	{
+		for (x = area->x1; x <= area->x2; x++)
+		{
+			bool color = lv_color_brightness(*color_p) < 128;
+			uint8_t col = x >> 3;
+			uint8_t pixel = x & 0x07;
+			uint8_t mask = 1 << (7 - pixel);
+			if (color) {
+				buf[col] |= mask;
+			}
+			else {
+				buf[col] &= ~mask;
+			}
+			color_p++;
+		}
+		glcdMono->WriteRow(y, buf, 30);
+	}
+}
+
+
+
--- display.h
+++ display.h
@@ -14,80 +14,15 @@ namespace ESP_LVGL
 	
 	class Display
 	{
-		GLCD* glcd = NULL;
+		GLCD_Mono* glcdMono = NULL;
 		uint8_t* buffer = NULL;
 		size_t bufferSize = 0;
 		lv_disp_draw_buf_t disp_buf;
 		lv_disp_drv_t disp_drv;
 		lv_disp_t* disp;
-		
-		void Flush_cb(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p)
-		{
-			if (glcd == NULL)
-				return;
-			int32_t x, y;
-			//Rounder ensures a whole line always needs to be updated.
-			uint8_t buf[30];	//30 columns
-			for (y = area->y1; y <= area->y2; y++) 
-			{
-				for (x = area->x1; x <= area->x2; x++)
-				{
-					bool color = lv_color_brightness(*color_p) < 128;
-					uint8_t col = x >> 3;
-					uint8_t pixel = x & 0x07;
-					uint8_t mask = 1 << (7 - pixel);
-					if (color) {
-						buf[col] |= mask;
-					}
-					else {
-						buf[col] &= ~mask;
-					}
-					color_p++;
-				}
-				glcd->WriteRow(y, buf, 30);
-			}
-		}
+		void Flush_cb_mono(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p);
 	public:
-		esp_err_t Init(GLCD* glcd)
-		{
-			if (glcd == NULL)
-				return ESP_FAIL;
-			this->glcd = glcd;
-			
-			uint32_t width = glcd->GetWidth();
-			uint32_t height = glcd->GetHeight();
-			
-			bufferSize = width * 10;
-			buffer = (uint8_t*)malloc(bufferSize);
-			if (buffer == NULL)
-				return ESP_FAIL;
-			
-			lv_disp_draw_buf_init(&disp_buf, buffer, NULL, bufferSize);
-			lv_disp_drv_init(&disp_drv);			
-			disp_drv.draw_buf = &disp_buf;			
-			disp_drv.hor_res = width;
-			disp_drv.ver_res = height;
-			disp_drv.user_data = this;
-			disp_drv.flush_cb = [](lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p) {
-				Display* instance = (Display*)disp_drv->user_data;
-				instance->Flush_cb(disp_drv, area, color_p);
-				lv_disp_flush_ready(disp_drv);
-			};
-	
-			//Always write complete lines.
-			disp_drv.rounder_cb = [](struct _lv_disp_drv_t * disp_drv, lv_area_t * a) {	
-				a->x1 = 0;
-				a->x2 = 240;	
-			};
-	
-			//disp_drv.set_px_cb = [](struct _lv_disp_drv_t * disp_drv, uint8_t * buf, lv_coord_t buf_w, lv_coord_t x, lv_coord_t y, lv_color_t color, lv_opa_t opa) {
-			// https://maldus512.medium.com/porting-littlevgl-for-a-monochrome-display-6c7be58851ce
-			// Not required
-			//};
-
-			disp = lv_disp_drv_register(&disp_drv); 
-			return ESP_OK;
-		}
+		esp_err_t Init(GLCD_Mono* glcdMonochrome);
 		
 		
 		
